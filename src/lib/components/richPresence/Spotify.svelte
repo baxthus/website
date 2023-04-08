99b1b3da1c0b5145dca942e831c6a12126b64e54
Abysmal <abysmal@ttm.sh>
2023-04-08 18:24:33 -0300

    feat(spotify): make spotify album art clickable

--- src/lib/components/richPresence/Spotify.svelte
+++ src/lib/components/richPresence/Spotify.svelte
@@ -2,6 +2,9 @@
     import type { LanyardData } from 'sk-lanyard';
 
     export let data: LanyardData;
+
+    const getLink = (track_id: string) =>
+        `https://open.spotify.com/track/${track_id}`;
 </script>
 
 {#if data?.spotify}
@@ -9,7 +12,7 @@
         <h1 class="font-bold">listening to</h1>
         <span class="text-[#f5c2e7]"
             ><a
-                href="https://open.spotify.com/track/{data.spotify.track_id}"
+                href={getLink(data.spotify.track_id)}
                 target="_blank"
                 rel="noreferrer"
                 class="hover:underline">{data.spotify.song}</a
@@ -17,13 +20,19 @@
         >
         <span class="text-[#89dceb]">{data.spotify.artist}</span>
         <span class="text-[#94e2d5]">{data.spotify.album}</span>
-        <img
-            src={data.spotify.album_art_url.replace(
-                'spotify:',
-                'https://i.scdn.co/image/',
-            )}
-            alt="Album cover"
-            class="w-36 mt-1"
-        />
+        <a
+            href={getLink(data.spotify.track_id)}
+            target="_blank"
+            rel="noreferrer"
+        >
+            <img
+                src={data.spotify.album_art_url.replace(
+                    'spotify:',
+                    'https://i.scdn.co/image/'
+                )}
+                alt="Album cover"
+                class="w-36 mt-1"
+            />
+        </a>
     </div>
 {/if}
