955a284eb43d05b2aad8d560493f5c89b2968e56
baxthus <baxt@baxt.fun>
2025-02-27 22:57:03 -0300

    fix: update ASCII art and add lyrics

--- src/server/plugins/art.ts
+++ src/server/plugins/art.ts
@@ -1,17 +1,21 @@
-/* eslint-disable no-irregular-whitespace */
 const art = `
 <!--
-            ＿＿
-　　　　　🌸＞　　フ
-　　　　　| 　_　 _|
-　 　　　／\` ミ_wノ
-　　 　 /　　　 　 |
-　　　 /　 ヽ　　 ﾉ
-　 　 │　　|　|　|
-　／￣|　　 |　|　|
-　| (￣ヽ＿_ヽ_)__)
-　＼二つ
+ ╱|、
+(˚ˎ 。7  
+|、˜〵          
+じしˍ,)ノ
+b-xth-s
 
+"""
+I want a perfect body, I want a perfect soul.
+I want you to notice when I'm not around.
+So fuckin' special, I wish I was special.
+But I'm a creep, I'm a weirdo.
+What the hell am I doing here?
+I don't belong here.
+"""
+
+I'm not okay :(
 -->
 `;
 
