0fcb13642b3e0ea0f6bb84057d4129aa761e100d
baxthus <baxt@baxt.fun>
2025-02-28 11:23:54 -0300

    fix: make Easter egg less edgy

--- src/server/plugins/art.ts
+++ src/server/plugins/art.ts
@@ -6,15 +6,6 @@ const art = `
 じしˍ,)ノ
 b-xth-s
 
-"""
-I want a perfect body, I want a perfect soul.
-I want you to notice when I'm not around.
-So fuckin' special, I wish I was special.
-But I'm a creep, I'm a weirdo.
-What the hell am I doing here?
-I don't belong here.
-"""
-
 I'm not okay :(
 -->
 `;
