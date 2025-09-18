815025cf71a3817e3bb3defac0c03e014008fd5d
baxthus <baxt@baxt.fun>
2025-09-18 18:02:58 -0300

    actual initial commit

---
+++ src/app/providers.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { HeroUIProvider, ToastProvider } from '@heroui/react';
+import { ThemeProvider } from 'next-themes';
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <HeroUIProvider>
+      <ThemeProvider attribute="class">
+        <ToastProvider />
+        {children}
+      </ThemeProvider>
+    </HeroUIProvider>
+  );
+}
