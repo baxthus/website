1072e6ab37c1b7572cb1a323e0c6578ae8ff09de
baxthus <root@baxt.dev>
2026-06-29 15:11:52 -0300

    fix(header): adjust links container height

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -10,7 +10,7 @@
 
 <header class="flex justify-between">
   <h1 class="block font-bold after:-mt-1.5 after:block after:content-['=======']">baxthus</h1>
-  <div class="flex">
+  <div class="flex h-fit">
     {#each links as link}
       <a class="font-bold" href={link.to}>{link.name}</a>
       {#if link !== links[links.length - 1]}
